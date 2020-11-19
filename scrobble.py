3074a6fb88f8d2885b6a2f7fe420e2956980f6d6
B0hr <trenson.gilles@gmail.com>
2020-11-19 14:23:46 +0100

    Added Hooray and De Tijdloze

--- scrobble.py
+++ scrobble.py
@@ -26,71 +26,59 @@ LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
 LASTFM_API_SECRET = os.getenv('LASTFM_API_SECRET')
 
 # Om te scrobblen moet je je natuurlijk ook inloggen
-lastfm_username = os.getenv('LASTFM_USERNAME')
-lastfm_password = pylast.md5(os.getenv('LASTFM_PASSWORD'))
+lastfm_username_bruut = os.getenv('LASTFM_USERNAME_BRUUT')
+lastfm_password_bruut = pylast.md5(os.getenv('LASTFM_PASSWORD_BRUUT'))
+lastfm_username_tijdloze = os.getenv('LASTFM_USERNAME_TIJDLOZE')
+lastfm_password_tijdloze = pylast.md5(os.getenv('LASTFM_PASSWORD_TIJDLOZE'))
+lastfm_username_hooray = os.getenv('LASTFM_USERNAME_HOORAY')
+lastfm_password_hooray = pylast.md5(os.getenv('LASTFM_PASSWORD_HOORAY'))
 
 # Studio Brussel Streams
 status = 'http://icecast.vrtcdn.be/status-json.xsl'
-stream = 'http://icecast.vrtcdn.be/stubru_bruut-mid.mp3'
+streams = ['bruut', 'tijdloze', 'hooray']
 
 def scrobble():
 
-    network = pylast.LastFMNetwork(
-        api_key = LASTFM_API_KEY, 
-        api_secret = LASTFM_API_SECRET,
-        username = lastfm_username, 
-        password_hash = lastfm_password
-    )
+    network = {}
+    for stream in streams:
+        network[stream] = pylast.LastFMNetwork(
+            api_secret = LASTFM_API_SECRET,
+            username = globals()[f'lastfm_username_{stream}'], 
+            password_hash = globals()[f'lastfm_password_{stream}']
+        )
 
     with requests.Session() as s:
-        # Voeg header toe om ook de metadata te bevragen
-        s.headers.update({
-            'Icy-MetaData': '1'
-        })
-        # Start de stream op
-        r = s.get(stream, stream=True)
-        # Controleer de encoding
-        if r.encoding is None:
-            r.encoding = 'utf-8'
-        # Loop over de data en zoek voor de songtitel
-        # De chunk_size is de grootte van het datablock
-        # Informatie te achterhalen via http://icecast.vrtcdn.be/status-json.xsl
-        for line in r.iter_content(chunk_size=19600, decode_unicode=True):
-            m = re.search(r'StreamTitle=\'(.*?) - (.*?)\';', line)
-            if m: 
-                artiest = m.group(1).lower()
-                nummer = m.group(2).lower()
-                logging.debug(f'Artiest: {artiest} - Nummer: {nummer}')
-                break
-        
-        # Haal de stream op uit de status pagina
+        # Haal de stream op uit de status pagina    
         r = s.get(status)
         # Controleer of je een geldige response kreeg
         if r.status_code == 200:
             json_data = r.json()
-            jsonpath_expr = parse('$.icestats.source[?server_name = "StuBru Bruut"].title')
-            matches = jsonpath_expr.find(json_data)
-            # Haal de artiest en het nummer op
-            for match in matches: 
-                m = re.search(r'(.*?) - (.*)', matches[0].value)
-                if m: 
-                    artiest = m.group(1).lower()
-                    nummer = m.group(2).lower()
-                    logging.debug(f'Artiest: {artiest} - Nummer: {nummer}')
-                    break
-
-    # Scrobble het nummer naar last.fm
-    user = network.get_authenticated_user()
-    current = network.get_track(artiest, nummer)
-    previous = user.get_recent_tracks(1, cacheable=False)[0].track
-
-    if current.get_correction() != previous.title:
-        network.scrobble(artiest, nummer, timestamp=time.time())
-        logging.debug(f'Nummer gescrobbled: {artiest.capitalize()} - {nummer.capitalize()}')
-    else:
-        logging.debug(f'Geen nieuw nummer...')
-
-    network.update_now_playing(artiest, nummer)
+            # Loop over de verschillende streams
+            for stream in streams:
+                jsonpath_expr = parse(f'$.icestats.source[?server_name =~ "{stream.capitalize()}"].title')
+                matches = jsonpath_expr.find(json_data)
+                # Definieer artiest en nummer
+                artiest, nummer = '', ''
+                # Haal de artiest en het nummer op
+                for match in matches: 
+                    m = re.search(r'(.*?) - (.*)', matches[0].value)
+                    if m: 
+                        artiest = m.group(1).lower()
+                        nummer = m.group(2).lower()
+                        logging.debug(f'StuBru {stream.capitalize} - Artiest: {artiest} - Nummer: {nummer}')
+                        break
+                # Scrobble de nummer naar last.fm
+                user = network[stream].get_authenticated_user()
+                current = network[stream].get_track(artiest, nummer)
+                previous = user.get_recent_tracks(1, cacheable=False)[0].track
+                # Scrobble het nummer als het niet het laatste nummer is
+                if current.get_correction() != previous.title:
+                    network[stream].scrobble(artiest, nummer, timestamp=time.time())
+                    logging.debug(f'Nummer gescrobbled naar StuBru-{stream.capitalize}: {artiest.capitalize()} - {nummer.capitalize()}')
+                else:
+                    logging.debug(f'Geen nieuw nummer...')
+                # Update now-playing
+                network[stream].update_now_playing(artiest, nummer)
 
 # In[2]: Start het clock process
 
